a674941c734f16150c715db4227cd523cf7ec074
Mathew Kellogg <mathew.kellogg@optimizely.com>
2015-08-23 21:06:53 -0700

    Initial Commit

---
+++ Tip With Friends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip With Friends
+//
+//  Created by Mathew Kellogg on 8/23/15.
+//  Copyright (c) 2015 Mathew Kellogg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
